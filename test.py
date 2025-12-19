e50a93be09528d38f93a37f940ba5cff7241c813
Iker Argulo <128476597+Dalinar69@users.noreply.github.com>
2025-12-19 13:04:28 +0100

    Implement test for add1000 method
    Add a test for the add1000 method in the Wallet class.

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_1000():
+    obj = Wallet(0)
+    obj.add1000()
+    asseeret obj.get_balance() = 1000
